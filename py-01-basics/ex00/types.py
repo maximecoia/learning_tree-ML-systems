2f40f874bd0249d827277658811a518488e73c19
Maxime COIA <coiamaxime@gmail.com>
2026-09-03 12:29:24 +0200

    isinstance instead of type

--- py-01-basics/ex00/types.py
+++ py-01-basics/ex00/types.py
@@ -9,3 +9,12 @@ def texte(valeur):
 
 def est_entier(valeur): 
     return type(valeur) == int
+
+print(entier("14"))
+print(entier("007"))
+print(flottant("14"))
+print(flottant("2.5"))
+print(texte(14))
+print(texte(2.5))
+print(est_entier(14))
+print(est_entier(14.0))
