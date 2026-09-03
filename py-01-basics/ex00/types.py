8d148cfc9fde6bb03cd8aaa937e3aa4129351464
Maxime COIA <coiamaxime@gmail.com>
2026-09-03 12:18:22 +0200

    ex0

---
+++ py-01-basics/ex00/types.py
@@ -0,0 +1,11 @@
+def entier(texte): 
+    return int(texte)
+
+def flottant(texte): 
+    return float(texte)
+
+def texte(valeur): 
+    return str(valeur)
+
+def est_entier(valeur): 
+    return type(valeur) == int
