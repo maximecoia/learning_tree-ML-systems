0282ee1e99fbba8cb1dd95e1e0738be9036f5d79
Maxime COIA <coiamaxime@gmail.com>
2026-09-03 12:50:13 +0200

    operateurs

---
+++ py-01-basics/ex01/operateurs.py
@@ -0,0 +1,16 @@
+def moyenne_de_deux(a, b): 
+    return (a + b) / 2
+
+def division_entiere(a, b): 
+    return a // b
+
+def reste(a, b): return a % b
+
+def puissance(base, exposant): 
+    return base ** exposant
+
+def ecart(a, b): 
+    return abs(a - b)
+
+def pourcentage(partie, total): 
+    return round(partie / total * 100, 2)
