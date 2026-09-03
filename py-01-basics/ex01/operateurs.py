88fa429fc8fc3a7117a26bf93f4b299d4e8005d4
Maxime COIA <coiamaxime@gmail.com>
2026-09-03 12:52:42 +0200

    operateurs

--- py-01-basics/ex01/operateurs.py
+++ py-01-basics/ex01/operateurs.py
@@ -14,3 +14,16 @@ def ecart(a, b):
 
 def pourcentage(partie, total): 
     return round(partie / total * 100, 2)
+
+print(moyenne_de_deux(3, 4))
+print(moyenne_de_deux(4, 4))
+print(division_entiere(7, 2))
+print(division_entiere(-7, 2))
+print(reste(7, 2))
+print(reste(-7, 2))
+print(puissance(2, 10))
+print(puissance(9, 0.5))
+print(ecart(3, 10))
+print(ecart(10, 3))
+print(pourcentage(1, 3))
+print(pourcentage(14, 20))
