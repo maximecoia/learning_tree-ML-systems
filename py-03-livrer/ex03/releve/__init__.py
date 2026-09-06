56afc1645b6c501feefdd6760bccf3f977a8c3f7
Maxime COIA <coiamaxime@gmail.com>
2026-09-06 18:39:13 +0200

    ex03

---
+++ py-03-livrer/ex03/releve/__init__.py
@@ -0,0 +1,14 @@
+from .mesures import centile95, depouiller, illisibles, lire, resumer
+from .cli import analyser, main, traiter
+
+
+__all__ = [
+    "lire",
+    "illisibles",
+    "depouiller",
+    "centile95",
+    "resumer",
+    "analyser",
+    "traiter",
+    "main",
+]
